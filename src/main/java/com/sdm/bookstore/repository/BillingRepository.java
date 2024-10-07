571254c71d95a9f1c8aaf7046ee089df710e2522
Sourabh Mahale <SourabhMahale98@gmail.com>
2024-10-07 22:59:52 +0530

    first commit

---
+++ src/main/java/com/sdm/bookstore/repository/BillingRepository.java
@@ -0,0 +1,11 @@
+package com.sdm.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sdm.bookstore.model.Customer;
+
+@Repository
+public interface BillingRepository extends CrudRepository<Customer, Long> {
+
+}
